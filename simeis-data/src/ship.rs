64295f63f1a73c25c068fb72336bd4f4266c0589
Valentin-Droid <valentin.drelon@icloud.com>
2025-06-27 10:54:42 +0200

    :bug; fix: Using cargo fmt for fix unproperly formatted

--- simeis-data/src/ship.rs
+++ simeis-data/src/ship.rs
@@ -27,7 +27,7 @@ const FUEL_TANK_CAP_PRICE: f64 = 30.0;
 const CARGO_CAP_PRICE: f64 = 20.0;
 const HULL_DECAY_CAP_PRICE: f64 = 9.0;
 const REACTOR_POWER_PRICE: f64 = 4000.0;
-const SHIELD_PRICE : f64 = 2500.0;
+const SHIELD_PRICE: f64 = 2500.0;
 
 const REACTOR_SPEED_PER_POWER: f64 = 50.0;
 
@@ -160,7 +160,8 @@ impl Ship {
     // Updates the performances of the ship based on the crew onboard
     pub fn update_perf_stats(&mut self) {
         self.stats = ShipStats::default();
-        self.stats.hull_usage_rate = HULL_USAGE_BASE / (1.0 + (1.0 + self.shield_power as f64).log(3.5));
+        self.stats.hull_usage_rate =
+            HULL_USAGE_BASE / (1.0 + (1.0 + self.shield_power as f64).log(3.5));
         self.stats.fuel_consumption = self.reactor_power as f64;
 
         if let Some(ref pilot) = self.pilot {
